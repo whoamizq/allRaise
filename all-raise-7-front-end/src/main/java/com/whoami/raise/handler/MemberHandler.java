44c8151d949a4107d79e1976c0eeda15abcfa54f
iamZQ <1726891762@qq.com>
2020-08-22 18:13:17 +0800

    前端登录功能Controller方法实现

--- all-raise-7-front-end/src/main/java/com/whoami/raise/handler/MemberHandler.java
+++ all-raise-7-front-end/src/main/java/com/whoami/raise/handler/MemberHandler.java
@@ -1,17 +1,43 @@
 package com.whoami.raise.handler;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+import com.whoami.raise.api.MemberManagerRemoteService;
+import com.whoami.raise.entity.ResultEntity;
+import com.whoami.raise.entity.vo.MemberSignSuccessVO;
+import com.whoami.raise.entity.vo.MemberVO;
+import com.whoami.raise.util.RaiseConstant;
+
+@Controller(value = "/member")
 public class MemberHandler {
+	@Autowired
+	private MemberManagerRemoteService memberManagerRemoteService;
 	
 	/**
-	 * 前往登录页面
+	 * 登录
+	 * @param memberVO
+	 * @param model
+	 * @param session
 	 * @return
 	 */
-	@RequestMapping("/to/login/page.html")
-	public String toLoginPage() {
-		return "member-login";
+	@RequestMapping(value = "/to/login.html")
+	public String doLogin(MemberVO memberVO,Model model,HttpSession session) {
+		// 调用远程方法执行登录操作
+		ResultEntity<MemberSignSuccessVO> resultEntity = memberManagerRemoteService.login(memberVO.getLoginacct(), memberVO.getUserpswd());
+		// 检查远程方法调用结果
+		if(ResultEntity.FAILED.equals(resultEntity.getResult())) {
+			model.addAttribute(RaiseConstant.ATTR_NAME_MESSAGE,resultEntity.getMessage());
+			return "member-login";
+		}
+		// 登录成功
+		MemberSignSuccessVO memberSignSuccessVO = resultEntity.getData();
+		// 将对象存入session域
+		session.setAttribute(RaiseConstant.ATTR_NAME_LOGIN_ADMIN, memberSignSuccessVO);
+		return "redirect:/member/to/member/center/page.html";
 	}
 }
