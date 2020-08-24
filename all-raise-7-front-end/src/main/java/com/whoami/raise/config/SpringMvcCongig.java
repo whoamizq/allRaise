c6a45c0fe1dbd9610a045a236d90073109ac4120
iamZQ <1726891762@qq.com>
2020-08-24 21:11:14 +0800

    前端-实现跳转到同意协议页面

--- all-raise-7-front-end/src/main/java/com/whoami/raise/config/SpringMvcCongig.java
+++ all-raise-7-front-end/src/main/java/com/whoami/raise/config/SpringMvcCongig.java
@@ -15,13 +15,17 @@ public class SpringMvcCongig implements WebMvcConfigurer{
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		// 相当于：<mvc:view-controller path="/to/login/page.html" view-name="member-login" />
-		
+		// 跳转到登录页面
 		String urlPath = "/member/to/login/page.html";
 		String viewName = "member-manager";
 		registry.addViewController(urlPath).setViewName(viewName);
-		
+		// 跳转到个人中心
 		urlPath = "/member/to/member/center/page.html";
 		viewName = "member-center";
+		registry.addViewController(urlPath).setViewName(viewName);
+		// 跳转到同意协议页面
+		urlPath = "/project/to/agree/page";
+		viewName = "project-1-start";
 		registry.addViewController(urlPath).setViewName(viewName);
 	}
 }
