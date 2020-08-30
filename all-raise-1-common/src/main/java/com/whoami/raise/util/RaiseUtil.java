8d7dcc9236dd9929a0106da653b50a07d20e7f2b
iamZQ <1726891762@qq.com>
2020-08-30 09:48:30 +0800

    上传单个文件到oss工具方法

--- all-raise-1-common/src/main/java/com/whoami/raise/util/RaiseUtil.java
+++ all-raise-1-common/src/main/java/com/whoami/raise/util/RaiseUtil.java
@@ -1,5 +1,6 @@
 package com.whoami.raise.util;
 
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
@@ -7,10 +8,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import javax.management.RuntimeErrorException;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
 
 import com.aliyun.api.gateway.demo.util.HttpUtils;
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSException;
 
 public class RaiseUtil {
 	
@@ -34,6 +40,35 @@ public class RaiseUtil {
 	 */
 	public static String generateFolderNameByDate(String ossProjectParentFolder) {
 		return ossProjectParentFolder+"/"+new SimpleDateFormat("yyyyMMdd").format(new Date());
+	}
+	
+	/**
+	 * 上传单个文件到OSS
+	 * @param endpoint 
+	 * @param accessKeyId 密钥id
+	 * @param accessKeySecret 密钥
+	 * @param fileName 文件名
+	 * @param folderName 目录
+	 * @param bucketName 存储空间名
+	 * @param inputStream 流
+	 */
+	public static void uploadSingleFile(String endpoint, String accessKeyId, String accessKeySecret, 
+			String fileName,String folderName, String bucketName, InputStream inputStream) {
+		try {
+			OSSClient ossClient = new OSSClient(endpoint, accessKeyId,accessKeySecret);
+			//存入对象的名称=目录名称+"/"+文件名
+			String objectName = folderName + "/" +fileName;
+			ossClient.putObject(bucketName, objectName, inputStream);
+			// 关闭
+			ossClient.shutdown();
+		}catch (OSSException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e.getMessage());
+		}catch (ClientException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e.getMessage());
+		}
+		
 	}
 	
 	/**
